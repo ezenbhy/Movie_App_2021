0206412041d573ebc0f469e9618ea433e308ee70
ezenbhy <ezenbhy@gmail.com>
2021-11-05 13:18:41 +0900

    리액트 무비 앱 업로드

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-  rootElement
+  <App />,
+  document.getElementById('root')
 );
 
